44c4d2df013cb59cbe4c207808e41bbb4d5b57e7
Krunoslav Zaher <krunoslav.zaher@gmail.com>
2017-11-07 15:16:44 +0100

    Adds `Signal`.

---
+++ SharedSequence/src/main/kotlin/org/notests/sharedsequence/SignalTraits.kt
@@ -0,0 +1,23 @@
+package org.notests.sharedsequence
+
+/**
+ * Created by kzaher on 11/7/17.
+ */
+
+import io.reactivex.Observable
+import io.reactivex.Scheduler
+import io.reactivex.android.schedulers.AndroidSchedulers
+import org.notests.sharedsequence.annotations.SharedSequence
+
+/**
+ * Created by markotron on 11/6/17.
+ */
+
+@SharedSequence("Signal")
+object SignalTraits : SharingTrait {
+    override val scheduler: Scheduler
+        get() = AndroidSchedulers.mainThread()
+
+    override fun <Element> share(source: Observable<Element>): Observable<Element> =
+            source.share()
+}
