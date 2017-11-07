16158c480e26a6375596b5c8da1b2c148b08f158
Krunoslav Zaher <krunoslav.zaher@gmail.com>
2017-11-07 15:17:08 +0100

    Adds simple usage to example app.

--- app/src/main/java/org/notests/sharedsequenceexample/MainActivity.kt
+++ app/src/main/java/org/notests/sharedsequenceexample/MainActivity.kt
@@ -2,11 +2,17 @@ package org.notests.sharedsequenceexample
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import org.notests.sharedsequence.Signal
+import org.notests.sharedsequence.just
 
 class MainActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+
+    Signal.just("Hello SharedSequence.kt! NoTests for you!").asObservable().subscribe({
+      println(it)
+    })
   }
 }
