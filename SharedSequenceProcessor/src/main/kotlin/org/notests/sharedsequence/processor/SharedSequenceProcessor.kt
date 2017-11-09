c44cdcc8e69b77b02da9190c9d7895abb4a32d37
Krunoslav Zaher <krunoslav.zaher@gmail.com>
2017-11-09 14:27:39 +0100

    Removes duplicated code from generator.

--- SharedSequenceProcessor/src/main/kotlin/org/notests/sharedsequence/processor/SharedSequenceProcessor.kt
+++ SharedSequenceProcessor/src/main/kotlin/org/notests/sharedsequence/processor/SharedSequenceProcessor.kt
@@ -9,16 +9,27 @@ import javax.lang.model.element.TypeElement
 import javax.tools.Diagnostic
 import java.net.URI
 
-class SharedSequenceProcessor : AbstractProcessor() {
+
+fun processTemplate(template: String, packageName: String, replacements: HashMap<String, String>): String {
+  fun setPackageName(template: String, packageName: String): String =
+          (listOf("package " + packageName) + template.split("\n", "\r").drop(1)).joinToString("\n")
+
   fun getTemplateContent(template: String): String {
-      try {
-        return URI("file:/Users/kzaher/Projects/SharedSequence.kt/SharedSequenceProcessor/src/main/resources/$template").toURL().readText()
-      }
-      catch(e: Exception) {
-        return javaClass.classLoader.getResource(template).readText()
-      }
+    try {
+      // horrible hack because for some reason build environment caches templates
+      // help needed
+      return URI("file:/Users/kzaher/Projects/SharedSequence.kt/SharedSequenceProcessor/src/main/resources/$template").toURL().readText()
+    } catch (e: Exception) {
+      return SharedSequenceProcessor.javaClass.classLoader.getResource(template).readText()
+    }
   }
 
+  return setPackageName(getTemplateContent(template), packageName).replace(Regex("_(\\w|\\d)+_")) {
+    replacements[it.value]!!
+  }
+}
+
+class SharedSequenceProcessor : AbstractProcessor() {
   override fun process(annotations: MutableSet<out TypeElement>, env: RoundEnvironment): Boolean {
 
     processingEnv.messager.printMessage(Diagnostic.Kind.NOTE, "Started")
@@ -31,18 +42,19 @@ class SharedSequenceProcessor : AbstractProcessor() {
       val objectName = el.simpleName.toString()
       val sharedSequenceName = el.getAnnotation(SharedSequence::class.java).value
 
-      val source = getTemplateContent("template.kt")
+      var replacements = hashMapOf(
+              "_Template_" to sharedSequenceName,
+              "_scheduler_" to "$objectName.scheduler",
+              "_share_" to "$objectName.share"
+      )
 
-      val replacedSource = source.replace("_Package_", pckg)
-        .replace("_Template_", sharedSequenceName)
-        .replace("_scheduler_", "$objectName.scheduler")
-        .replace("_share_", "$objectName.share")
+      val source = processTemplate("template.kt", pckg, replacements)
 
-      processingEnv.messager.printMessage(Diagnostic.Kind.NOTE, replacedSource)
+      //processingEnv.messager.printMessage(Diagnostic.Kind.NOTE, replacedSource)
 
       File("$kotlinGenerated/$objectName.kt/${pckg.replace(".", "/")}", "${sharedSequenceName}.kt").apply {
         parentFile.mkdirs()
-        writeText(replacedSource)
+        writeText(source)
       }
     }
 
@@ -60,14 +72,16 @@ class SharedSequenceProcessor : AbstractProcessor() {
 
         val pckg = processingEnv.elementUtils.getPackageOf(el1).qualifiedName.toString()
 
-        val source = getTemplateContent("templatetemplate.kt")
-        val replacedSource = source.replace("_Package_", pckg)
-          .replace("_Template_", sharedSequenceName1)
-          .replace("_Template2_", sharedSequenceName2)
+        var replacements = hashMapOf(
+                "_Template_" to sharedSequenceName1,
+                "_Template2_" to sharedSequenceName2
+        )
+
+        val source = processTemplate("templatetemplate.kt", pckg, replacements)
 
         File("$kotlinGenerated/$objectName1+$objectName2.kt/${pckg.replace(".", "/")}", "$sharedSequenceName1+$sharedSequenceName2.kt").apply {
           parentFile.mkdirs()
-          writeText(replacedSource)
+          writeText(source)
         }
       }
 
@@ -78,7 +92,7 @@ class SharedSequenceProcessor : AbstractProcessor() {
   override fun getSupportedSourceVersion(): SourceVersion = SourceVersion.latestSupported()
   override fun getSupportedOptions() = setOf(GENERATE_KOTLIN_CODE_OPTION)
 
-  private companion object {
+  companion object {
     val GENERATE_KOTLIN_CODE_OPTION = "generate.kotlin.code"
     val KAPT_KOTLIN_GENERATED_OPTION = "kapt.kotlin.generated"
   }
