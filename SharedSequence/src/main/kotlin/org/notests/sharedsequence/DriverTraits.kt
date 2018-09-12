078e06296ec720a1783424bba165170458902c3f
Marko Bozic <markotron@gmail.com>
2018-09-12 20:16:44 +0200

    [Improvement] Drive method now doesn't ignore exceptions but propagates to ErrorReporting
    Summary:
        - Now we have a try-catch around the `onNext` arguemnt in the
        `drive` method.
        - In this way if you throw in the drive method, we'll not
        unsubscribe. We'll report the exception via ErrorReporting and
        continue.
    
    Test plan:
        - We added a test case convering the inteneded scenario

--- SharedSequence/src/main/kotlin/org/notests/sharedsequence/DriverTraits.kt
+++ SharedSequence/src/main/kotlin/org/notests/sharedsequence/DriverTraits.kt
@@ -5,6 +5,7 @@ import io.reactivex.Observer
 import io.reactivex.Scheduler
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
+import org.notests.sharedsequence.api.ErrorReporting
 import org.notests.sharedsequence.api.SharedSequence
 import org.notests.sharedsequence.api.SharingTrait
 
@@ -20,8 +21,18 @@ object DriverTraits : SharingTrait {
     source.replay(1).refCount()
 }
 
-fun <Element> Driver<Element>.drive(onNext: (Element) -> Unit = {}): Disposable =
-  this.asObservable().subscribe(onNext, {})
+fun <Element> Driver<Element>.drive(onNext: (Element) -> Unit = {}): Disposable = this
+  .asObservable()
+  .subscribe {
+    try {
+      onNext(it)
+    } catch (e: Exception) {
+      ErrorReporting.report(e)
+    }
+  }
 
+/**
+ * Subscribe with a custom observer. Keep in mind that you have to handle errors manually here.
+ */
 fun <Element> Driver<Element>.drive(observer: Observer<Element>): Unit =
   this.asObservable().subscribe(observer)
